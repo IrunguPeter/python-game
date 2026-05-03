52a39eb9429b14da8275801723ee29b7557f61fd
Peter Irungu <irungupeter204@gmail.com>
2026-05-03 10:44:34 +0300

    added numpy and matplotlib

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 
 #this is a guess game
 # There is competition and the one with the fewest guesses wins
@@ -10,7 +12,7 @@ for i in range(20):
     y=int(input("Enter your Lucky number   "))
 
     if x==y:
-        print(f"You got it in {i} attempts ")
+        print(f"You got it in {i+1} attempts ")
         break
     
     elif x>y:
