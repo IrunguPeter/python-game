fc902c14905f8a5e9a3a804aa8b5f826519083e7
Peter Irungu <irungupeter204@gmail.com>
2026-05-03 11:14:08 +0300

    Refactor: A bit of competition

--- game.py
+++ game.py
@@ -5,17 +5,28 @@ import matplotlib.pyplot as plt
 #this is a guess game
 # There is competition and the one with the fewest guesses wins
 
-x=random.randrange(1,100)
-#x is a random integer between 1 and 100
+d=int(input("Enter the number of players  "))
 
-for i in range(20):
-    y=int(input("Enter your Lucky number   "))
+for x in range(d):
+    n=input("Enter your name   ")
 
-    if x==y:
-        print(f"You got it in {i+1} attempts ")
-        break
-    
-    elif x>y:
-        print("Too low")
-    else:
-        print("Too high")
+    x=random.randrange(1,100)
+    #x is a random integer between 1 and 100
+
+    for i in range(20):
+        y=int(input("Select your lucky number between 1 and 100   "))
+
+        if x==y:
+            print(f"You got it in {i+1} attempts ")
+            e=np.array(1+1)
+            f=np.array(n)
+
+            plt.plot(e,f)
+
+            plt.show()
+            break
+
+        elif x>y:
+            print("Too low")
+        else:
+            print("Too high")
