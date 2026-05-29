7d668cb71deec984403eb9b58237633d4d57923c
Peter Irungu <irungupeter204@gmail.com>
2026-05-29 07:52:39 +0300

    Added bar graphs for easy comparison between players

--- game.py
+++ game.py
@@ -1,14 +1,18 @@
 import random
-import numpy as np
+import array as arr
 import matplotlib.pyplot as plt
 
 #this is a guess game
 # There is competition and the one with the fewest guesses wins
 
 d=int(input("Enter the number of players  "))
+Scores=[]
+Names=[]
 
 for x in range(d):
-    n=input("Enter your name   ")
+    N=input("Enter your name   ")
+    Names.append(N)
+
 
     x=random.randrange(1,100)
     #x is a random integer between 1 and 100
@@ -18,15 +22,15 @@ for x in range(d):
 
         if x==y:
             print(f"You got it in {i+1} attempts ")
-            e=np.array(1+1)
-            f=np.array(n)
-
-            plt.plot(e,f)
-
-            plt.show()
+            e=(1+1)
+            Scores.append(e)
+                  
             break
 
         elif x>y:
             print("Too low")
         else:
             print("Too high")
+
+plt.bar(Names, Scores)
+plt.show()
